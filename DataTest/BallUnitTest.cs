d822bceaa9452b8c4ca4568556a0b72db2e5a5b4
258675 Przemysław Walczak <walczakprzemek77@gmail.com>
2025-05-12 01:16:03 +0200

    test fix

--- DataTest/BallUnitTest.cs
+++ DataTest/BallUnitTest.cs
@@ -10,36 +10,14 @@
 
 namespace TP.ConcurrentProgramming.Data.Test
 {
-  [TestClass]
-  public class BallUnitTest
-  {
-    [TestMethod]
-    public void ConstructorTestMethod()
+    [TestClass]
+    public class BallUnitTest
     {
-      Vector testinVector = new Vector(0.0, 0.0);
-      Ball newInstance = new(testinVector, testinVector, 400, 400, 10);
-    }
-
         [TestMethod]
-        public void MoveTestMethod()
+        public void ConstructorTestMethod()
         {
-            Vector initialPosition = new(10.0, 10.0);
-            Ball newInstance = new(initialPosition, new Vector(0.0, 0.0), 100, 100, 10);
-            IVector curentPosition = new Vector(0.0, 0.0);
-            int numberOfCallBackCalled = 0;
-
-            newInstance.NewPositionNotification += (sender, position) =>
-            {
-                Assert.IsNotNull(sender);
-                curentPosition = position;
-                numberOfCallBackCalled++;
-            };
-
-            newInstance.Move(); 
-
-            Assert.AreEqual(1, numberOfCallBackCalled);
-            Assert.AreEqual(initialPosition.x, curentPosition.x);
-            Assert.AreEqual(initialPosition.y, curentPosition.y);
+            Vector testinVector = new Vector(0.0, 0.0);
+            Ball newInstance = new(testinVector, testinVector, 400, 400, 10);
         }
 
     }
