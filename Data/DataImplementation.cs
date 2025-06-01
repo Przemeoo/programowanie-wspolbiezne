ede5b7add2714656ff6bc2492aae75c9265bca63
258675 Przemysław Walczak <walczakprzemek77@gmail.com>
2025-06-01 20:27:13 +0200

    added real-time programming elements

--- Data/DataImplementation.cs
+++ Data/DataImplementation.cs
@@ -58,7 +58,7 @@ namespace TP.ConcurrentProgramming.Data
                     throw new InvalidOperationException("Nie można znaleźć wolnej pozycji dla kulki po maksymalnej liczbie prób.");
                 }
 
-                Vector initialVelocity = new Vector((random.NextDouble() - 0.5) * 6, (random.NextDouble() - 0.5) * 6);
+                Vector initialVelocity = new Vector((random.NextDouble() - 0.5) * 150, (random.NextDouble() - 0.5) * 150);
 
                 Ball newBall = new(startingPosition, initialVelocity);
                 upperLayerHandler(startingPosition, newBall);
